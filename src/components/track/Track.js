0d19c77c41c2def0547be6e0e85b21a068003085
Skultty <samuel.skultety@outlook.sk>
2022-09-23 22:02:34 +0200

    Same as the commit before but I also linked components between the source files

---
+++ src/components/track/Track.js
@@ -0,0 +1,26 @@
+import React from "react";
+import "./Track.css";
+
+export class Track extends React.Component {
+
+    renderAction() {
+        if (this.props.isRemoval) {
+            return <a className="Track-action">-</a>
+        } else {
+            return <a className="Track-action">+</a>
+        }
+    }
+    
+
+    render() {
+        return (
+            <div className="Track">
+                <div className="Track-information">
+                    <h3>track name will go here</h3>
+                    <p>track artist will go here | track album will go here</p>
+                </div>
+                <a className="Track-action">+</a>
+            </div>
+        );
+    }
+}
