1cf783a82a548c76560562d42b9682d1bc4d3eb3
Skultty <samuel.skultety@outlook.sk>
2022-09-23 20:55:25 +0200

    Replaced favicon Added new css file Deleted the not needed App.test.js file

---
+++ src/components/app/App.js
@@ -0,0 +1,21 @@
+
+import './App.css';
+import React from 'react';
+import { SearchBar } from '../searchBar/SearchBar';
+
+ class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Ja<span className="highlight">mmm</span>ing</h1>
+        <div className="App">
+
+          <div className="App-playlist">
+
+          </div>
+        </div>
+    </div>
+    );
+  }
+}
+export default App;
